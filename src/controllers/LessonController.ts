bda3d820b519f42898bc41a87173aab5b170b181
Hemal99 <h.githmin@gmail.com>
2023-05-12 06:40:20 +0530

    update

--- src/controllers/LessonController.ts
+++ src/controllers/LessonController.ts
@@ -2,25 +2,36 @@ import { plainToClass } from "class-transformer";
 import { CreateLessonInput, SubjectEnum } from "../dto";
 import { Request, Response, NextFunction } from "express";
 import { Lesson } from "../models/Lesson";
+import { Role } from "../utility/constants";
 
 export const createLesson = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const lessonInputs = plainToClass(CreateLessonInput, req.body);
+  try {
+    const lessonInputs = plainToClass(CreateLessonInput, req.body);
 
-  const { lessonDescription, lessonName, subject } = lessonInputs;
+    const { name, description, subject, thumbnail } = lessonInputs;
 
-  const lesson = new Lesson({
-    lessonDescription,
-    lessonName,
-    subject,
-  });
+    const user = req.user;
 
-  const result = await lesson.save();
+    if (user && user.role === Role.Admin) {
+      const lesson = new Lesson({
+        lessonName: name,
+        lessonDescription: description,
+        lessonThumbnail: thumbnail,
+        subject,
+      });
 
-  return res.status(201).json(result);
+      const result = await lesson.save();
+      return res.status(201).json(result);
+    }
+
+    return res.status(401).json({ msg: "Not Authorized" });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
 export const getChemistryLessons = async (
@@ -28,17 +39,44 @@ export const getChemistryLessons = async (
   res: Response,
   next: NextFunction
 ) => {
-  const lessons = await Lesson.find({ subject: SubjectEnum.CHEMISTRY });
-  return res.status(200).json(lessons);
+  try {
+    const lessons = await Lesson.find({ subject: SubjectEnum.CHEMISTRY });
+    return res.status(200).json(lessons);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
 export const getPhysicsLessons = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-    ) => {
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
     const lessons = await Lesson.find({ subject: SubjectEnum.PHYSICS });
     return res.status(200).json(lessons);
-    };
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
 
-    
+// get All Lessons
+
+export const getAllLessons = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = req.user;
+
+    if (user && user.role === Role.Admin) {
+      const lessons = await Lesson.find();
+      return res.status(200).json(lessons);
+    }
+
+    return res.status(401).json({ msg: "Error while Fetching Lessons" });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
