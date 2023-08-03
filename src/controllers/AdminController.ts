274c5b54d55cf9672ed8b92d5e33a626ebdf2b5b
Hemal99 <h.githmin@gmail.com>
2023-08-03 10:51:07 +0530

    update

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -135,7 +135,7 @@ export const GetStudentPackPayments = async (
 
     if (user && user.role === Role.Admin) {
       const profiles = await StudyPackPayment.find({}).populate(
-        "studyPackUserId"
+        "studyPackUserId studyPackId" 
       );
 
       if (profiles) {
